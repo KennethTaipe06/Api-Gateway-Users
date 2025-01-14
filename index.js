a538418529039528febb5f85a9ddb6c6afc396bb
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-01-13 20:55:32 -0500

    Agregar configuración inicial del API Gateway con soporte para CORS y proxies a servicios externos

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+require('dotenv').config(); // Cargar variables de entorno
+
+const app = express();
+
+app.use(cors()); // Agregar soporte para CORS
+
+app.use('/reset-password', createProxyMiddleware({ target: process.env.RESET_PASSWORD_SERVER, changeOrigin: true }));
+app.use('/api/users', createProxyMiddleware({ target: process.env.REGISTER_SERVER, changeOrigin: true }));
+app.use('/users', createProxyMiddleware({ target: process.env.FETCH_PROFILE_SERVER, changeOrigin: true }));
+app.use('/users', createProxyMiddleware({ target: process.env.UPDATE_PROFILE_SERVER, changeOrigin: true }));
+app.use('/users', createProxyMiddleware({ target: process.env.DELETE_PROFILE_SERVER, changeOrigin: true }));
+app.use('/api/auth/login', createProxyMiddleware({ target: process.env.LOGIN_SERVER, changeOrigin: true }));
+app.use('/check-email', createProxyMiddleware({ target: process.env.FORGOT_PASSWORD_SERVER, changeOrigin: true }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => { // Escuchar en todas las interfaces de red
+  console.log(`API Gateway running on port ${PORT}`);
+});
