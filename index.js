c3fff5b713511f2c7a3e8288736fcf77870cfbb3
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-01-18 13:05:33 -0500

    feat: Agregar ruta de logout al API Gateway

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use('/users', createProxyMiddleware({ target: process.env.FETCH_PROFILE_SERV
 app.use('/users', createProxyMiddleware({ target: process.env.UPDATE_PROFILE_SERVER, changeOrigin: true }));
 app.use('/users', createProxyMiddleware({ target: process.env.DELETE_PROFILE_SERVER, changeOrigin: true }));
 app.use('/api/auth/login', createProxyMiddleware({ target: process.env.LOGIN_SERVER, changeOrigin: true }));
+app.use('/api/auth/logout', createProxyMiddleware({ target: process.env.LOGIN_SERVER, changeOrigin: true }));
 app.use('/check-email', createProxyMiddleware({ target: process.env.FORGOT_PASSWORD_SERVER, changeOrigin: true }));
 
 const PORT = process.env.PORT || 5000;
